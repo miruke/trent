bf43d94d0d9a7ec5250ba152e883c38e6044586e
miruke <155471165+miruke@users.noreply.github.com>
2024-05-29 23:08:24 +0300

    Initial Commit

---
+++ trent Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trent Watch App
+//
+//  Created by Ren Zhongkai on 5/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trent Watch App/trentApp.swift
@@ -0,0 +1,17 @@
+//
+//  trentApp.swift
+//  trent Watch App
+//
+//  Created by Ren Zhongkai on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct trent_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
