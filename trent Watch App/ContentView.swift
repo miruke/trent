da22afd090ed1449998f784cf08b933e6bc1dcfe
miruke <155471165+miruke@users.noreply.github.com>
2024-06-02 00:39:36 +0300

    Load schedule from file

--- trent Watch App/ContentView.swift
+++ trent Watch App/ContentView.swift
@@ -6,30 +6,33 @@
 //
 
 import SwiftUI
+import Foundation
 
+func readDataFromFileToArray(atPath filePath: String) -> [String] {
+    let url = Bundle.main.url(forResource: filePath, withExtension: "txt")
+    let path = Bundle.main.url(forResource: filePath, withExtension: "txt")!.path
+    do {
+        let dataFromFile = try String(contentsOfFile: path, encoding: .utf8)
+        
+        let dataArray = dataFromFile.components(separatedBy: "\n")
+        
+        return dataArray
+    } catch {
+        print("Error reading data: \(error.localizedDescription)")
+        return []
+    }
+}
 
 struct ContentView: View {
-    let items = ["12:00 U",
-                 "12:03 U",
-                 "12:04 UA",
-                 "12:05 E",
-                 "12:06 E",
-                 "12:12 E",
-                 "12:22 E",
-                 "12:30 E",
-                 "12:32 E",
-                 "12:36 E",
-                 "12:44 E",
-                 "12:47 E",
-                 "12:54 E",
-                 "12:58 E",
-    ]
-    @State private var scrollTarget: Int? = nil
+    let items = readDataFromFileToArray(atPath:"lep")
     
     func findItem(hh: Int, mm: Int) -> Int? {
         // iterate through array
         if let index = items.firstIndex(where: { item in
             let itemString = String(item)
+            if itemString.count < 6 {
+                return false
+            }
             let h = Int(itemString.prefix(2))!
             let m = Int(itemString.prefix(5).suffix(2))!
             return h == hh && m >= mm
@@ -47,9 +50,9 @@ struct ContentView: View {
             }
             .listStyle(.carousel)
             .onTapGesture {
-                if let index = findItem(hh:12, mm:32) {
+                if let index = findItem(hh:7, mm:22) {
                     withAnimation {
-                        proxy.scrollTo(items[index], anchor: .top) // Scroll to the 5th item (index 4)
+                        proxy.scrollTo(items[index], anchor: .top)
                     }
                 }
             }
